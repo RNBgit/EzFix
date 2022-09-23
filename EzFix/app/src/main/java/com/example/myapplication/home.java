b86262bc18bbdbf18be34bbc44ecb0b778b23166
RNBgit <reevbelt@gmail.com>
2022-09-23 21:03:20 +0800

    EzFix App

---
+++ EzFix/app/src/main/java/com/example/myapplication/home.java
@@ -0,0 +1,60 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.denzcoskun.imageslider.ImageSlider;
+import com.example.myapplication.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class home extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    HomeFragment homeFragment = new HomeFragment();
+    ChatFragment chatFragment = new ChatFragment();
+    NotificationFragment notificationFragment = new NotificationFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, homeFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected( MenuItem item) {
+
+                switch (item.getItemId()){
+
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, homeFragment).commit();
+                        return true;
+                    case R.id.chat:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, chatFragment).commit();
+                        return true;
+                    case R.id.notify:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, notificationFragment).commit();
+                        return true;
+                    case R.id.profile:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, profileFragment).commit();
+                        return true;
+
+                }
+
+
+                return false;
+            }
+        });
+
+    }
+}
